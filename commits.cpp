d5e8ff1798693bdb98db095181e7e48b79e50a05
Дарья <dasasor2006@gmail.com>
2025-03-23 20:25:49 +0400

    Второй коммит - вывод n-ного числа Фибоначи

--- commits.cpp
+++ commits.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+long long fibonacci(int n, vector<long long>& fibok) {
+    if (n <= 0) return 0;
+    else if (n == 1) return 1;
+    fibok[n] = fibonacci(n - 1, fibok) + fibonacci(n - 2, fibok);
+    return fibok[n];
+}
+
 int main() {
-    cout << "Hello, World!";
+    int n;
+    cin >> n;
+    vector<long long> fibok(n + 1, -1);
+    long long result = fibonacci(n, fibok);
+    cout << result << endl;
+    return 0;
+
 }
