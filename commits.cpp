100d3e256bc1c404a78a66d01dc0aa6be05ea28d
Дарья <dasasor2006@gmail.com>
2025-03-23 20:29:34 +0400

    Третий коммит - вывод n-ного числа Фибоначи с условием: первый элемент равен 0

--- commits.cpp
+++ commits.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 long long fibonacci(int n, vector<long long>& fibok) {
     if (n <= 0) return 0;
-    else if (n == 1) return 1;
+    else if (n == 1) return 0;
+    else if (n == 2) return 1;
     fibok[n] = fibonacci(n - 1, fibok) + fibonacci(n - 2, fibok);
     return fibok[n];
 }
